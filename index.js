62a33e19c48cf4ca0f8a68ff2a590cdf6e498c5b
Jake Robbins <jrobbinsd@gmail.com>
2017-11-04 17:39:41 -0600

    landing page in progress

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const queries = require('./db/queries')
 
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) =>{
   res.render('index');
 })
 
+app.get('/todo',(req, res) =>{
+	knex
+	.return
+})
 
 
 app.listen(port, () =>{
